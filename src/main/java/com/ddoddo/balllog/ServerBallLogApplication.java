258e378620d0b80006dc8917e63a6331e4b9a689
hyeongiii <hyeongiii0811@gmail.com>
2025-01-13 15:56:12 +0900

    :tada: init: 프로젝트 생성

---
+++ src/main/java/com/ddoddo/balllog/ServerBallLogApplication.java
@@ -0,0 +1,13 @@
+package com.ddoddo.balllog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerBallLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerBallLogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ddoddo/balllog/ServerBallLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddoddo.balllog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerBallLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
