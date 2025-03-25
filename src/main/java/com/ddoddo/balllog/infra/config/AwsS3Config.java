9904b7e4f80a529cad23221e93950edc5fc4174f
hyeongiii <hyeongiii0811@gmail.com>
2025-03-25 16:31:16 +0900

    :sparkles: feat: S3 presignedUrl 생성 API

---
+++ src/main/java/com/ddoddo/balllog/infra/config/AwsS3Config.java
@@ -0,0 +1,47 @@
+package com.ddoddo.balllog.infra.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+
+/**
+ * AWS SDK V2 Config
+ */
+@Configuration
+public class AwsS3Config {
+
+    @Value("${cloud.aws.region.static}")
+    private String regionName;
+
+    @Bean
+    public AwsCredentialsProvider awsCredentialsProvider() {
+        return DefaultCredentialsProvider.create();
+    }
+
+    @Bean
+    public Region region() {
+        // 추후 변경 예정
+        return Region.of(regionName);
+    }
+
+    @Bean
+    public S3Client s3Client(AwsCredentialsProvider credentialsProvider, Region region) {
+        return  S3Client.builder()
+                .region(region)
+                .credentialsProvider(credentialsProvider)
+                .build();
+    }
+
+    @Bean
+    public S3Presigner s3Presigner(AwsCredentialsProvider credentialsProvider, Region region) {
+        return S3Presigner.builder()
+                .region(region)
+                .credentialsProvider(credentialsProvider)
+                .build();
+    }
+}
